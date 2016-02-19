c70217541a83fb752a25e9597d454cc38a97edae
altunbas-huseyin <altunbas.huseyin@gmail.com>
2016-02-20 01:19:50 +0200

    User.js Password bcrypt paketi ile şifrelendi.
    bcrypt,node-uuid npm paketleri eklendi.
    User.js Model’de bcrypt  ile Password şifrelendi.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -15,3 +15,4 @@ module.exports = require('waterlock').actions.user({
     }
   */
 });
+
--- api/models/User.js
+++ api/models/User.js
@@ -19,3 +19,80 @@ module.exports = {
   beforeCreate: require('waterlock').models.user.beforeCreate,
   beforeUpdate: require('waterlock').models.user.beforeUpdate
 };
+
+
+
+var bcrypt = require('bcrypt');
+var uuid = require('node-uuid');
+var SALT_WORK_FACTOR = 10;
+
+module.exports = {
+  
+  attributes: {
+
+    name: {
+      type: 'string',
+      required: true,
+      minLength: 3,
+      maxLength: 30
+    },
+
+   
+
+    email:{ 
+      type: 'email',
+      required: true,
+      unique: true
+    },
+
+    password:{
+      type: 'string',
+      required: true,
+      minLength: 6,
+      maxLength: 50
+    },
+
+
+    verifyPassword: function (password) {
+      return bcrypt.compareSync(password, this.password);
+    },
+
+    changePassword: function(newPassword, cb){
+      this.newPassword = newPassword;
+      this.save(function(err, u) {
+        return cb(err,u);
+      });
+    },
+
+    toJSON: function() {
+      var obj = this.toObject();
+      return obj;
+    }
+  },
+
+  beforeCreate: function (attrs, cb) {
+    bcrypt.hash(attrs.password, SALT_WORK_FACTOR, function (err, hash) {
+      attrs.password = hash;
+      return cb();    
+    });
+  },
+
+  beforeUpdate: function (attrs, cb) {
+    if(attrs.newPassword){
+      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return cb(err);
+
+        bcrypt.hash(attrs.newPassword, salt, function(err, crypted) {
+          if(err) return cb(err);
+
+          delete attrs.newPassword;
+          attrs.password = crypted;
+          return cb();
+        });
+      });
+    }
+    else {
+      return cb();
+    }
+  }
+};
